c72aaefc2c835391d6cd055229791ca067377191
Dima <darenth0@gmail.com>
2021-02-28 18:36:53 +0100

    initial commit1

---
+++ src/test/java/pages/Profile.java
@@ -0,0 +1,31 @@
+package pages;
+
+
+import org.hamcrest.core.IsEqual;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+
+public class Profile extends BasePage {
+
+    public Profile() {
+        super();
+    }
+
+    @FindBy(css = "#center_column > h1")
+    private WebElement profilePageHeader;
+
+    @Step
+    public void userShouldBeSuccessfullyRegistered() {
+        String profilePageHeaderText = profilePageHeader.getText();
+        String expectedHeaderText = "MY ACCOUNT";
+
+        assertEquals(profilePageHeaderText, IsEqual.equalTo(expectedHeaderText));
+    }
+}
